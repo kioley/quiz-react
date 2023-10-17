211d11eebde5f899b0c7293c37344b95f451bfc8
kioley <kiioley@gmail.com>
2023-10-17 09:47:04 +0300

    ref: getting rid of the state router

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,26 @@ import { useViewStore } from "./store/viewStore.ts"
 export function App() {
   const screen = useViewStore((s) => s.screen)
 
+  let view
+
+  switch (screen) {
+    case Screens.Menu:
+      view = <Menu />
+      break
+    case Screens.Quiz:
+      view = <Quiz />
+      break
+    case Screens.Win:
+      view = <Win />
+      break
+    case Screens.Defeat:
+      view = <Defeat />
+  }
+
   return (
     <div className="w-screen h-screen bg-img-main bg-no-repeat bg-top flex justify-center bg-cover">
       <div className="h-full w-[calc(55vh)] flex flex-col items-center p-3 [container-type:inline-size] justify-between">
-        <StateRouter screen={screen}>
-          <Route element={<Menu />} screen={Screens.Menu} />
-          <Route element={<Quiz />} screen={Screens.Quiz} />
-          <Route element={<Win />} screen={Screens.Win} />
-          <Route element={<Defeat />} screen={Screens.Defeat} />
-        </StateRouter>
+        {view}
       </div>
     </div>
   )
